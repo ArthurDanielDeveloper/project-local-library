3e1075c54a704e3693bced9e4bb7098cb32fb6f3
ArthurDanielDeveloper <73367799+ArthurDanielDeveloper@users.noreply.github.com>
2020-12-17 06:23:17 -0700

    Initial upload
    First upload since passing qualified tests

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,43 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  return accounts.find((account) => account.id === id)
+}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  return accounts.sort(( lastName1, lastName2) => lastName1.name.last.toLowerCase() > lastName2.name.last.toLowerCase() ? 1 : -1)
+}
+//see if reduce works with Zach
+function numberOfBorrows(account, books) {
+  let borrowers = []
+ for (let i=0; i<books.length; i++){
+   for (let j=0; j<books[i].borrows.length; j++){
+     if (books[i].borrows[j].id === account.id){
+      borrowers.push(account.id)
+     }
+   }
+ }
+ console.log(borrowers)
+ return borrowers.length
+}
 
-function numberOfBorrows(account, books) {}
-
-function getBooksPossessedByAccount(account, books, authors) {}
+function getBooksPossessedByAccount(account, books, authors) {
+  let possessedBooks = [];
+  for (let i=0; i<books.length; i++){
+    let book = books[i];
+    const {id, title, genre, borrows}=book;
+    for (let j=0; j<borrows.length; j++){
+      if (borrows[j].id === account.id && borrows[j].returned === false){
+        for (let k=0; k<authors.length; k++){
+          let author = authors[k];
+          if (author.id === book.authorId){
+            let tempBook = {id, title, genre, author, borrows}
+            possessedBooks.push(tempBook);
+          }
+        }
+      }
+    }
+  }
+  return possessedBooks;
+}
 
 module.exports = {
   findAccountById,
--- public/src/books.js
+++ public/src/books.js
@@ -1,11 +1,34 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  return authors.find((author) => author.id === id)
+}
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  return books.find((book)=> book.id === id)
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  let bookStatus = []
+  let booksLoaned = []
+  let booksReturned = []
+  booksLoaned = books.filter((book) => book.borrows[0].returned === false)
+  booksReturned = books.filter((book) => book.borrows[0].returned === true)
+  bookStatus.push(booksLoaned, booksReturned)
+  return bookStatus
+}
 
-function getBorrowersForBook(book, accounts) {}
-
+function getBorrowersForBook(book, accounts) {
+  let borrowers = [];
+  accounts.forEach(account =>{
+    book.borrows.forEach(transaction =>{
+      if (transaction.id === account.id){
+        let accountObj = {...account}
+        accountObj.returned = transaction.returned;
+        borrowers.push(accountObj)
+      }
+    })
+  })
+return borrowers.slice(0,10)
+}
 module.exports = {
   findAuthorById,
   findBookById,
